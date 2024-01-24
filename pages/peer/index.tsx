7799054385334888601007ffad6743e699c9b396
VitaliiPolushkin <v.polushkin@digiscorp.com>
2024-01-24 03:24:36 +0100

    initialize peer and set connection between two peers

--- pages/peer/index.tsx
+++ pages/peer/index.tsx
@@ -1,105 +1,71 @@
-import { useAppDispatch } from "@/redux";
+import { useAppDispatch, useTypedSelector } from "@/redux";
 import { getAuth } from "@/redux/slices/userSlice";
 import { Box, Button, Card } from "@mui/material";
 import Peer from "peerjs";
-import { FC, useEffect, useState } from "react";
-
-// export const createPeerInstance = (id: string) => {
-//   let peer: any;
-//   if (typeof window !== "undefined") {
-//     import("peerjs").then(({ default: Peer }) => {
-//       if (Peer) {
-//         peer = new Peer(id);
-//       }
-//     });
-
-//     return peer;
-//   }
-//   return peer;
-// };
+import { FC, FormEvent, useEffect, useState } from "react";
 
 interface PeerProps {}
 
 const PeerPage: FC<PeerProps> = () => {
-  const [id, setId] = useState("");
-  const [connectedId, setConnectedId] = useState("");
+  const [userName, setUserName] = useState("");
   const [peer, setPeer] = useState<Peer | undefined>();
+
+  const { user } = useTypedSelector((state) => state.user);
 
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(getAuth(null));
+    dispatch(getAuth());
   }, [dispatch]);
 
   useEffect(() => {
-    // if (typeof window !== "undefined") {
-    //   const { Peer } = require("peerjs"); // or import Peer from 'peerjs';
-    //   const peer = new Peer("id");
-    //   console.log(`peer id: ${peer.id}`);
-    //   //connect to other peer
-    //   const connection = peer.connect("other peer id");
-    //   peer.on("call", (call) => {});
-    //   // rest of the useEffect code
-    //   return () => {
-    //     // cleanup code
-    //   };
-    // }
-    if (typeof window !== "undefined") {
-      if (peer) {
-        peer.on("call", (call) => {
-          call.answer(new MediaStream());
-          console.log("call", call);
-        });
-      }
-    }
+    if (!peer) return console.log("Effect: no peer");
+
+    peer.on("connection", () => {
+      console.log("peer connected");
+    });
   }, [peer]);
 
-  function createPeer() {
+  const initPeer = () => {
     if (typeof window !== "undefined") {
       if (peer) {
         return console.log("peer already created");
       }
       const { Peer } = require("peerjs"); // or import Peer from 'peerjs';
 
-      const newPeer = new Peer(id);
-      setPeer(newPeer);
+      if (user?.firstName) {
+        const newPeer = new Peer(user?.firstName);
+        setPeer(newPeer);
+      }
     }
-  }
+  };
 
-  const connectToPeer = () => {
-    if (typeof window !== "undefined") {
-      if (!connectedId) return console.log("no id");
+  const connect = (e: FormEvent) => {
+    e.preventDefault();
 
-      if (!peer) return console.log("no connected id");
-      peer.connect(connectedId);
-      peer.call(connectedId, new MediaStream());
+    if (!peer) return console.log("no peer");
 
-      console.log("peer", peer);
-    }
+    peer.connect(userName).on("open", () => {
+      console.log("open");
+    });
   };
 
   return (
     <Box component={"section"} minHeight={"100vh"}>
-      {peer && peer.id}
+      user: {user?.firstName}
+      peer: {peer && peer.id}
       <Card>
-        <label htmlFor="peerId">Create Peer and setId</label>
-        <input
-          type="text"
-          id="peerId"
-          value={id}
-          onChange={(e) => setId(e.target.value)}
-        />
-        <Button onClick={createPeer}>Create Peer</Button>
-      </Card>
-      <Card>
-        <label htmlFor="peerId2">Connect to Peer</label>
-        <input
-          type="text"
-          id="peerId2"
-          value={connectedId}
-          onChange={(e) => setConnectedId(e.target.value)}
-        />
-        <Button onClick={connectToPeer}>Connect to</Button>
+        <Button onClick={initPeer}>Init Peer</Button>
+        <form onSubmit={connect}>
+          <label htmlFor="userName">set name</label>
+          <input
+            type="text"
+            id="userName"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <Button type="submit">Connect</Button>
+        </form>
       </Card>
     </Box>
   );
